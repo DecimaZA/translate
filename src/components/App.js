d98e679c35befb18e386204374e739fe821aacd8
Jonathan Wright <devorakwright@gmail.com>
2022-03-21 15:39:54 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
+import UserCreate from "./UserCreate";
+
+class App extends React.Component {
+    state = { language: 'english' };
+
+    onLanguageChange = (language) => {
+        this.setState({ language: language });
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                Select a language: 
+                <div>
+                    
+                    <i className="flag us" 
+                        onClick={() => this.onLanguageChange('english')}
+                    />
+                    <i className="flag nl" 
+                        onClick={() => this.onLanguageChange('dutch') } 
+                    />
+                </div>
+                <LanguageContext.Provider value={this.state.language} >
+                    <UserCreate />
+                    </LanguageContext.Provider>
+            </div>
+        );
+    }
+}
+
+export default App;
